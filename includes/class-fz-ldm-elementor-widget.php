6f62bbc789347806cdc62f7c41bf755ea24dead4
riku45100 <riku45100@users.noreply.github.com>
2026-09-01 00:59:07 +0300

    Add sun and moon icons to toggle thumb

--- includes/class-fz-ldm-elementor-widget.php
+++ includes/class-fz-ldm-elementor-widget.php
@@ -14,9 +14,9 @@ class Elementor_Widget extends \Elementor\Widget_Base{
   $this->add_control('dark_label',['label'=>'Dark label','type'=>\Elementor\Controls_Manager::TEXT,'default'=>'Dark','condition'=>['show_labels'=>'yes']]);
   $this->end_controls_section();
   $this->start_controls_section('style',['label'=>'Toggle Style','tab'=>\Elementor\Controls_Manager::TAB_STYLE]);
-  $this->add_control('track_color',['label'=>'Light track','type'=>\Elementor\Controls_Manager::COLOR,'selectors'=>['{{WRAPPER}} .fz-toggle'=>'background-color:{{VALUE}};']]);
-  $this->add_control('dark_track_color',['label'=>'Dark track','type'=>\Elementor\Controls_Manager::COLOR,'selectors'=>['{{WRAPPER}} .fz-toggle'=>'--fz-dark-track:{{VALUE}};']]);
-  $this->add_control('thumb_color',['label'=>'Switch thumb','type'=>\Elementor\Controls_Manager::COLOR,'selectors'=>['{{WRAPPER}} .fz-toggle__thumb'=>'background-color:{{VALUE}};']]);
+  $this->add_control('light_track',['label'=>'Light track','type'=>\Elementor\Controls_Manager::COLOR,'selectors'=>['{{WRAPPER}} .fz-toggle'=>'background-color:{{VALUE}};']]);
+  $this->add_control('dark_track',['label'=>'Dark track','type'=>\Elementor\Controls_Manager::COLOR,'selectors'=>['{{WRAPPER}} .fz-toggle'=>'--fz-dark-track:{{VALUE}};']]);
+  $this->add_control('thumb_color',['label'=>'Thumb colour','type'=>\Elementor\Controls_Manager::COLOR,'selectors'=>['{{WRAPPER}} .fz-toggle__thumb'=>'background-color:{{VALUE}};']]);
   $this->add_responsive_control('width',['label'=>'Width','type'=>\Elementor\Controls_Manager::SLIDER,'size_units'=>['px'],'range'=>['px'=>['min'=>32,'max'=>100]],'default'=>['unit'=>'px','size'=>54],'selectors'=>['{{WRAPPER}} .fz-toggle'=>'width:{{SIZE}}{{UNIT}};']]);
   $this->add_responsive_control('height',['label'=>'Height','type'=>\Elementor\Controls_Manager::SLIDER,'size_units'=>['px'],'range'=>['px'=>['min'=>20,'max'=>60]],'default'=>['unit'=>'px','size'=>30],'selectors'=>['{{WRAPPER}} .fz-toggle'=>'height:{{SIZE}}{{UNIT}};']]);
   $this->add_responsive_control('thumb_size',['label'=>'Thumb size','type'=>\Elementor\Controls_Manager::SLIDER,'size_units'=>['px'],'range'=>['px'=>['min'=>12,'max'=>50]],'default'=>['unit'=>'px','size'=>22],'selectors'=>['{{WRAPPER}} .fz-toggle__thumb'=>'width:{{SIZE}}{{UNIT}};height:{{SIZE}}{{UNIT}};']]);
@@ -26,7 +26,7 @@ class Elementor_Widget extends \Elementor\Widget_Base{
  protected function render(){ $s=$this->get_settings_for_display(); $labels=($s['show_labels']??'')==='yes'; ?>
  <div class="fz-ldm-widget fz-ldm-toggle-wrap" role="group" aria-label="Colour mode">
  <?php if($labels): ?><span class="fz-toggle-label fz-toggle-label--light"><?php echo esc_html($s['light_label']); ?></span><?php endif; ?>
- <button type="button" class="fz-toggle" data-fz-toggle aria-label="Toggle dark mode" aria-pressed="false"><span class="fz-toggle__thumb"></span></button>
+ <button type="button" class="fz-toggle" data-fz-toggle aria-label="Switch to dark mode" aria-pressed="false"><span class="fz-toggle__icon" aria-hidden="true"><span class="fz-icon-sun">☀</span><span class="fz-icon-moon">☾</span></span><span class="fz-toggle__thumb"></span></button>
  <?php if($labels): ?><span class="fz-toggle-label fz-toggle-label--dark"><?php echo esc_html($s['dark_label']); ?></span><?php endif; ?>
  </div>
  <?php }
